70ab77faf14980e6f8a2fae99e6277064027037d
hyosikKim98 <85177334+hyosikKim98@users.noreply.github.com>
2025-12-05 17:25:34 +0900

    validation finish

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,32 +1,20 @@
 package hello.itemservice.domain.item;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-import org.hibernate.validator.constraints.Range;
 
-import java.util.List;
+import lombok.Data;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price + _this.quantity >= 10000")
 public class Item {
 
     private Long id;
-
-    @NotBlank
     private String itemName;
-
-    @NotNull
-    @Range(min = 1000, max = 1000000)
     private Integer price;
-
-    @NotNull
-    @Max(9999)
     private Integer quantity;
 
-    private Boolean open;
-    private List<String> regions;
-    private ItemType itemType;
-    private String deliveryCode;
+//    private Boolean open;
+//    private List<String> regions;
+//    private ItemType itemType;
+//    private String deliveryCode;
 
     public Item() {
     }
